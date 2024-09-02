879a020c126ef3163e48391c29e0591d6d024bdc
Okoli Frank <okolicharles104@gmail.com>
2024-09-02 14:20:59 +0100

    authentication 2.1

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,2 @@
 import { handlers } from "@/auth";
-export const{GET,POST}=handlers;
-
-import Google from "next-auth/providers/google"
- 
-export const {auth, signIn, signOut } = NextAuth({
-  providers: [
-    Google({
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-  ],
-})
+export const{GET,POST}=handlers
