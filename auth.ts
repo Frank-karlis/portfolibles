5a49dcf831670e81aad1cbe7077aa04522079022
Okoli Frank <okolicharles104@gmail.com>
2024-09-02 07:50:56 +0100

    copy previous codes

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { authConfig } from './auth.config';
 import GoogleProvider from "next-auth/providers/google";
 import TwitterProvider from "next-auth/providers/twitter";
 import FacebookProvider from "next-auth/providers/facebook";
+import {Auth} from "@auth/core"
  
 export const { handlers,auth,signIn,signOut } = NextAuth({
   ...authConfig,
@@ -14,12 +15,11 @@ export const { handlers,auth,signIn,signOut } = NextAuth({
     TwitterProvider({
         clientId:process.env.TWITTER_CLIENT_ID,
         clientSecret:process.env.TWITTER_CLIENT_SECRET,
-      
     }),
-    FacebookProvider({
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret:process.env.FACEBOOK_CLIENT_SECRET,
-    }),
+    // FacebookProvider({
+    //     clientId: process.env.FACEBOOK_CLIENT_ID,
+    //     clientSecret:process.env.FACEBOOK_CLIENT_SECRET,
+    // }),
 ],
 });
 
